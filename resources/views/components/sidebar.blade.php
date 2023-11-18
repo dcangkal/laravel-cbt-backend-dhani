9e26242ebc88aadfa29ae3eff37f90d296b97214
dcangkal <rr.dhani@gmail.com>
2023-11-18 10:28:58 +0800

    laravel, feature soals

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -25,5 +25,13 @@
                     </li>
                 </ul>
             </li>
+            <li class="nav-item dropdown">
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Soal</span></a>
+                <ul class="dropdown-menu">
+                    <li class=''>
+                        <a class="nav-link" href="{{ route('soal.index') }}">All Soal</a>
+                    </li>
+                </ul>
+            </li>
     </aside>
 </div>
